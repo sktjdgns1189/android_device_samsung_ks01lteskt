ee5eaaba921ac5482025459f089b19368d872dca
Say <sktjdgns1189@naver.com>
2016-01-27 02:12:14 +0900

    Remove deprecated HEVC decoder

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -67,7 +67,6 @@ done
 PRODUCT_PACKAGES += \\
     libacdbloader \\
     libaudioalsa \\
-    libHevcSwDecoder \\
     libtime_genoff \\
     libTimeService \\
     TimeService
@@ -110,16 +109,6 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := libaudioalsa
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/vendor/lib/libaudioalsa.so
-LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libHevcSwDecoder
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
 LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
